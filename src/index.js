7f5ab91900ea383ee8f76c490ffe73d1d40aba38
paulaperoutka <paulaperoutka@gmail.com>
2018-04-24 09:24:33 -0400

    minion json object

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Card from "./components/Card";
+import Nav from "./components/Nav";
+import Title from "./components/Title";
+
+class App extends Component {
+
+}
+
+render {
+
+}
+
+export default App;
+// ReactDOM.render(<App />, document.getElementById('root'));
+
